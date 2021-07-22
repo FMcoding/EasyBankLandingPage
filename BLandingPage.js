2b1a51785a8efdcc7da84560ddea9d37a4dc19b6
TestCode <68029426+FMcoding@users.noreply.github.com>
2021-07-21 23:55:55 -0400

    Add files via upload

--- BLandingPage.js
+++ BLandingPage.js
@@ -1,3 +1,14 @@
 $("a").click(function(e) {
     e.preventDefault();
 });
+
+if ($(window).width() <= 500) {
+    $(".menu").click(function(){
+        if ($(".mobile-menu").css("display") === "grid") {
+            $(".mobile-menu").css("display", "none");
+        } else {
+            $(".mobile-menu").css("display", "grid");
+        }
+        
+    });
+}
